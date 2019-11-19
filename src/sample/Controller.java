99d4282af9c3686d2741bfcc120c5ed65894ab08
wdz19910402@gmail.com <wdz19910402@gmail.com>
2019-11-19 09:06:46 -0800

    show image error

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
@@ -28,6 +29,8 @@ import java.util.List;
 
 public class Controller {
     Stage newWindow;
+    BorderPane bp;
+    List<File> fileList = new ArrayList<File>();
     public void uploadClick(){
         FileChooser fc2 = new FileChooser();
         FileChooser.ExtensionFilter addextension = new FileChooser.ExtensionFilter("image", "*.png", "*.jpg");
@@ -35,43 +38,44 @@ public class Controller {
         List<File> list = fc2.showOpenMultipleDialog(newWindow);
 
         if(list != null){
+            int row = 0;
+            int col = 0;
+            GridPane grid = new GridPane();
+            grid.setPadding(new Insets(10, 10, 10, 10));
+            grid.setVgap(20);
+            grid.setHgap(20);
+            
+            bp.setCenter(grid);
             for(File file: list){
                 String path = file.toURI().toString();
-                System.out.println(path);
-            }
+                Image img = new Image(path);
+                String[] splits = path.split("/");
+                String fileName = splits[splits.length - 1];
+
+
+                path = path.substring(6);
+                Mat imgcv = Imgcodecs.imread(path);
+                int height = imgcv.height();
+                int width = imgcv.width();
+                Label label1 = new Label("Image Name : " + fileName);
+                Label label2 = new Label(" Height : " + height + " Width: " + width);
+
+                ImageView imgView = new ImageView(img);
+                imgView.setFitHeight(100);
+                imgView.setFitWidth(100);
+
+                fileList.add(file);
+                VBox vbox = new VBox(imgView, label1, label2);
+                GridPane.setConstraints(vbox, col, row);
+                grid.getChildren().add(vbox);
+                col++;
+                if(col == 3){
+                    row++;
+                    col = 0;
+                    }
+                }
+        } else{
+            System.out.println("Something is wrong!");
         }
-//            int row = 0;
-//            int col = 0;
-//            for(File file: list){
-//                String path = file.toURI().toString();
-//                Image img = new Image(path);
-//                String[] splits = path.split("/");
-//                String fileName = splits[splits.length - 1];
-//
-//
-//                path = path.substring(6);
-//                Mat imgcv = Imgcodecs.imread(path);
-//                int height = imgcv.height();
-//                int width = imgcv.width();
-//                Label label1 = new Label("Image Name : " + fileName);
-//                Label label2 = new Label(" Height : " + height + " Width: " + width);
-//
-//                ImageView imgView = new ImageView(img);
-//                imgView.setFitHeight(100);
-//                imgView.setFitWidth(100);
-//
-//                fileList.add(file);
-//                VBox vbox = new VBox(imgView, label1, label2);
-//                GridPane.setConstraints(vbox, col, row);
-//                grid.getChildren().add(vbox);
-//                col++;
-//                if(col == 3){
-//                    row++;
-//                    col = 0;
-//                    }
-//                }
-//        } else{
-//            System.out.println("Something is wrong!");
-//        }
     }
 }
